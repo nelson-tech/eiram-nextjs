29ccd99a6c3bc8988e1de3084a388bdd80038e63
Michael Nelson <revnelson@gmail.com>
2023-02-12 02:57:53 -0700

    Modify Link component to fix forwardRef issue.
    Signed-off-by: Michael Nelson <revnelson@gmail.com>

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -1,39 +1,20 @@
-import { HTMLProps } from "react"
 import NextLink from "next/link"
-
+import type { LinkProps } from "next/link"
 // ####
 // #### Types
 // ####
 
-export type PropsType = HTMLProps<HTMLAnchorElement> & { passHref?: boolean }
+export type LinkPropsType = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
+	LinkProps & {
+		children?: React.ReactNode
+	} & React.RefAttributes<HTMLAnchorElement>
 
 // ####
 // #### Component
 // ####
 
-const Link = ({
-	href,
-	title,
-	target,
-	className,
-	children,
-	onClick,
-	passHref = false,
-	...rest
-}: PropsType) => {
-	return (
-		<NextLink
-			href={href || ""}
-			title={title}
-			target={target}
-			onClick={onClick}
-			passHref
-			id="nextlink"
-			className={className}
-		>
-			{children}
-		</NextLink>
-	)
+const Link = ({ children, ...linkProps }: LinkPropsType) => {
+	return <NextLink {...linkProps}>{children}</NextLink>
 }
 
 export default Link
