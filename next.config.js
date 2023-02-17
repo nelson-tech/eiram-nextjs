3ec4eb28a13e56c95e84d3e2ecfeeae58b6c7f69
Michael Nelson <revnelson@gmail.com>
2023-02-17 01:32:20 -0700

    Add cross-fetch to next config server bundle.
    - Update .prettierrc and .eslintrc
    
    Signed-off-by: Michael Nelson <revnelson@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		appDir: true,
-	},
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-		domains: ["cdn.eiramknitwear.com", "api.eiramknitwear.com", "shy.nelson.tech"],
-		loader: "custom",
-		loaderFile: "lib/utils/imageLoader.ts",
-	},
-	compiler: {
-		removeConsole: process.env.NODE_ENV === "production",
-	},
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["cross-fetch"],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "cdn.eiramknitwear.com",
+      "api.eiramknitwear.com",
+      "eiramknitwear.com",
+    ],
+    loader: "custom",
+    loaderFile: "lib/utils/imageLoader.ts",
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 }
 
 module.exports = nextConfig
