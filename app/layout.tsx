0c6c25966ed207be0b10ce4a13919adb8cdad677
Michael Nelson <revnelson@gmail.com>
2023-04-02 08:23:32 +0000

    Add RevalidateButton component to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import Modals from "components/Layout/Modals"
 import Footer from "components/Layout/Footer"
 import Alerts from "components/Alerts"
 import ScrollToTop from "components/ScrollToTop"
+import RevalidateButton from "components/RevalidateButton"
 // import Analytics from "components/Analytics"
 
 const font = localFont({
@@ -45,6 +46,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
           <Alerts />
           <ScrollToTop />
           {/* <Analytics /> */}
+          <RevalidateButton />
         </RootClientContext>
       </body>
     </html>
